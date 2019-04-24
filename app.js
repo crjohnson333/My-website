8773e617d6bafa0870dd147bb8c9293296431ccd
adam driggers <awdriggs@gmail.com>
2019-04-24 17:47:07 -0400

    trash

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ var restaurants = new Vue({
   methods: {
     get: function() {
       //debugger;
-      var url = 'https://api.yelp.com/v3/businesses/search' + this.lookup;
-      var token = "8KFcr_gfumVPMD-8JcvG4PV3dBcNYNZ66PdjDPv59kqkDBZH2qjfX65mG3ID6WN8FkBArJwNu_M-1eHNu-YFSddCJme6YoRzXAJh7fUsWIlV0uS5nYwXfw8tf3CuXHYx"
-
+      //var url = 'https://api.yelp.com/v3/businesses/search' + this.lookup;
+      //var token = "8KFcr_gfumVPMD-8JcvG4PV3dBcNYNZ66PdjDPv59kqkDBZH2qjfX65mG3ID6WN8FkBArJwNu_M-1eHNu-YFSddCJme6YoRzXAJh7fUsWIlV0uS5nYwXfw8tf3CuXHYx"
+      var url = "http://api.yelp.com/business_review_search";
       axios.get(url, {
-        mode: 'no-cors',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          Authorization: 'Bearer ' + token
-        }
+        responseType: "json",
+        data: {term : 'restaurant', lat:40.6, long: -73.9 }, 
       }).then(function(response){
         debugger;
         vue.restaurants = response.data.data
